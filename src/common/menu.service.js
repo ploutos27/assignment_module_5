7fe4cbace750b9cb80f74b9789081ed7688b9c34
Ploutarchos Michaelides <anymvs@gmail.com>
2020-06-07 22:13:45 +0300

    add info and singup

--- src/common/menu.service.js
+++ src/common/menu.service.js
@@ -8,6 +8,16 @@ angular.module('common')
 MenuService.$inject = ['$http', 'ApiPath'];
 function MenuService($http, ApiPath) {
   var service = this;
+  service.user = {};
+
+  service.saveUser = function(user) {
+    service.user = angular.copy(user);
+    console.log(service.user);
+  }
+
+  service.getUser = function() {
+    return service.user;
+  }
 
   service.getCategories = function () {
     return $http.get(ApiPath + '/categories.json').then(function (response) {
@@ -27,6 +37,10 @@ function MenuService($http, ApiPath) {
     });
   };
 
+  service.getFavoriteDish = function(short_name) {
+    return $http.get(ApiPath + '/menu_items/' + short_name + '.json');
+  }
+  
 }
 
 
